b1f0cc32884a90a7878b3ea51ba8f413481889a4
sergo13377 <138023861+sergo13377@users.noreply.github.com>
2025-05-14 16:34:54 +0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import PlausibleProvider from "next-plausible";
 import "./globals.css";
 
-let title = "Llama Coder – AI Code Generator";
-let description = "Generate your next app with Llama 3.1 405B";
-let url = "https://llamacoder.io/";
-let ogimage = "https://llamacoder.io/og-image.png";
-let sitename = "llamacoder.io";
+let title = "MakeApp – AI Code Generator";
+let description = "Generate your next app with MakeAppAI";
+let url = "https://MakeAppAI.org/";
+let ogimage = "https://MakeAppAI.org/og-image.png";
+let sitename = "MakeAppAI.org";
 
 export const metadata: Metadata = {
   metadataBase: new URL(url),
